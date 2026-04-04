cfb28833dfe0e441396f75702849f3f4a579a7e0
Areena Mahek <63315474+AreenaMahek@users.noreply.github.com>
2026-04-03 20:46:25 -0500

    Update basePath and assetPrefix for production

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/AreenaMahek",
-  assetPrefix: "/AreenaMahek/",
+  basePath: isProd ? "/AreenaMahek" : "",
+  assetPrefix: isProd ? "/AreenaMahek/" : "",
   images: {
     unoptimized: true,
   },
